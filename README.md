00cd5e1ec4fb281aebde96a360e755bb3bfe0403
Bram Oosterlynck <booster@inuits.eu>
2022-11-08 21:30:14 +0100

    Add current conditions page

---
+++ README.md
@@ -0,0 +1,16 @@
+# Weewx Console
+
+A CLI-friendly skin for weewx. Inspired by [wttr.in](https://github.com/chubin/wttr.in).
+Using this skin, you can query the status of your weather station using your favorite console HTTP client (curl, httpie, wget...).
+
+## Installation
+
+Clone the repo and run the installer:
+```
+wee_extension --install weewx-console
+```
+
+Restart weewx:
+```
+sudo systemctl restart weewx
+```
---
+++ install.py
@@ -0,0 +1,25 @@
+# Copyright 2022 Bram Oosterlynck
+
+from setup import ExtensionInstaller
+
+def loader():
+    return ConsoleInstaller()
+
+class ConsoleInstaller(ExtensionInstaller):
+    def __init__(self):
+        super(ConsoleInstaller, self).__init__(
+            version="0.1",
+            name='console',
+            description='A console-friendly layout.',
+            author="Bram Oosterlynck",
+            author_email="bram.oosterlynck@gmail.com",
+            config={
+                'StdReport': {
+                    'console': {
+                        'skin':'console',
+                        'HTML_ROOT':'console'}}},
+            files=[('skins/console',
+                    ['skins/console/current.tmpl',
+                     'skins/console/skin.conf'])
+                   ]
+            )
